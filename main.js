db012c2eedacf27e0f28fd21a04b3c87b5e1e295
risonakamo <hmykanae@gmail.com>
2018-05-23 13:14:15 -0400

    pixiv linkgen function

--- main.js
+++ main.js
@@ -39,3 +39,38 @@ function parseInput()
         _output.insertAdjacentHTML("beforeend",`<a href="${inputLinks[x]}" target="_blank" rel="noreferrer">${inputLinks[x]}</a>`);
     }
 }
+
+//give it the top page of a pixiv multi image to generate native pixiv
+//big image links
+//give it the direct source of an image to generate source links, it doesnt
+//matter which page it is
+function pixivGen(url,count)
+{
+    var toppagereg=/\?mode=medium/;
+    var srcreg=/img-original/;
+    var res="";
+
+    if (url.search(toppagereg)>0)
+    {
+        //the illustrator id
+        var illid=url.replace(/.*illust_id=(\d+)/,"$1");
+        for (var x=0;x<count;x++)
+        {
+            res+=`https://www.pixiv.net/member_illust.php?mode=manga_big&illust_id=${illid}&page=${x}\n`;
+        }
+    }
+
+    else if (url.search(srcreg)>0)
+    {
+        var preurl=url.match(/(.*_p)\d+\.(\w+)/);
+        var imgextension=preurl[2];
+        preurl=preurl[1];
+
+        for (var x=0;x<count;x++)
+        {
+            res+=`${preurl}${x}.${imgextension}\n`;
+        }
+    }
+
+    _inputBox.value=res;
+}
